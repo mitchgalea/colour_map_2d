5627b7be0ef6fa048b16bfb1e1fd95632c0291a6
mitchgalea <42560979+mitchgalea@users.noreply.github.com>
2019-02-28 08:58:11 +1100

    added mutex

--- include/colour_map_2d/grid.h
+++ include/colour_map_2d/grid.h
@@ -16,43 +16,46 @@ namespace ColourMap2D{
 class Grid
 {
 private:
+    ////MEMBERS
     std::vector<GridCell> grid_cells_;
     nav_msgs::MapMetaData grid_info_;
-
+    std::mutex mutex_;
+    ////FILTER MEMBERS
     double hit_prob_;
     double miss_prob_;
     double min_prob_;
-    
+    ////CELL STATE MEMBERS
     int cell_obstacle_;
     int cell_occupied_;
     int cell_empty_;
     int cell_unknown_;
     int frame_;
-    
+    ////SPAWN MEMBERS
     double spawn_noise_;
     int spawn_rate_;
     int spawn_size_;
-
+    ////BOOL MEMBERS
     bool initialized_;
     
+    ////PRIVATE METHODS
+    double findMaxProb(int spaces, int index);
 public:
+    ////CONSTRUCTORS
     Grid();
     Grid(double hit_prob, double miss_prob, double min_prob,
          int cell_occupied, int cell_obstacle, int cell_empty, int cell_unknown,
          int spawn_rate, double spawn_noise, int spawn_size, int frame);
 
     ////GETTERS
-    nav_msgs::MapMetaData getGridInfo() const;
     bool initialized() const;
 
     ////METHODS
     void processOGMap(const nav_msgs::OccupancyGrid &og_map);
     void initializeGrid(nav_msgs::MapMetaData og_map_data);
-    void proccessPoint(int index, uint8_t r, uint8_t g, uint8_t b);
+    //void proccessPoint(int index, uint8_t r, uint8_t g, uint8_t b);
     void proccessPoints(std::vector<ColourPoint> &points);
     void updateImage(cv::Mat &image);
     void initializeMapImage(cv::Mat &image);
-    double findMaxProb(int spaces, int index);
 };
 
 }
