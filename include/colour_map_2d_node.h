f75bfc17550b5633c6e19e38650e94cf3f898b10
mitchgalea <42560979+mitchgalea@users.noreply.github.com>
2019-03-06 10:34:35 +1100

    commented

--- include/colour_map_2d_node.h
+++ include/colour_map_2d_node.h
@@ -32,26 +32,35 @@
 #endif // COLOUR_MAP_2D_NODE_H
 
 namespace ColourMap2D{
-
+//ColourMap2D Node class
+//Subscribes to Occupancy Grid and Point Cloud
+//Publishes Map Image
 class ColourMap2DNode{
 private:
+    //ROS MEMBERS
     ros::NodeHandle nh_;
     ros::Subscriber og_sub_;
     ros::Subscriber pc_sub_;
     image_transport::ImageTransport it_;
     image_transport::Publisher image_pub_;
 
+    //MEMBERS
     Grid grid_;
     cv_bridge::CvImage colour_map_image_;
     double map_image_rate_;
-
+    
+    //TRANSFER MEMBERS
     tf2_ros::Buffer tf_buffer_;
     tf2_ros::TransformListener tf_listener_;
     std::string target_frame_;
 public:
+    //CONSTRUCTOR
+    //Constructor initializes subscribers and publishers as well as taking inputs for member variables
     ColourMap2DNode(ros::NodeHandle nh);
+    //ROS CALLBACKS
     void ogCallback(const nav_msgs::OccupancyGrid og);
     void pcCallback(const sensor_msgs::PointCloud2ConstPtr& pCloud);
+    //PUBLISH THREAD
     void mapImageThread();
 };
 }
