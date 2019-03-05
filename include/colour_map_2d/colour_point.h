48de5c33ccc88e80babca860452e30222ef1b776
mitchgalea <42560979+mitchgalea@users.noreply.github.com>
2019-03-06 09:02:49 +1100

    commented

--- include/colour_map_2d/colour_point.h
+++ include/colour_map_2d/colour_point.h
@@ -4,16 +4,18 @@
 #define COLOUR_POINT_H
 
 namespace ColourMap2D{
-
+//Class used to store transformed Point Cloud information in
 class ColourPoint{
-    double x_;
-    double y_;
-    uint8_t r_;
-    uint8_t g_;
-    uint8_t b_;
+    double x_;      //x pose in meters
+    double y_;      //y pose in meters  
+    uint8_t r_;     //r pixel value
+    uint8_t g_;     //g pixel value
+    uint8_t b_;     //b pixel value
 public:
     ////CONSTRUCTORS
+    //constructor that takes input for all variables
     ColourPoint(double x_in, double y_in, uint8_t r_in, uint8_t g_in, uint8_t b_in);
+    //constructor that generates a point based on another point with added noise
     ColourPoint(ColourPoint point, double x_noise, double y_noise);
     
     ////GETTERS
